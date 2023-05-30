8015a0e8928e45bd856d740e120fdcffb5010292
mcheng <mcheng@splunk.com>
2023-05-30 13:24:43 +0800

    added fluent logger

--- src/main/java/com/example/vulnerabilities/controller/pingController.java
+++ src/main/java/com/example/vulnerabilities/controller/pingController.java
@@ -1,15 +1,25 @@
 package com.example.vulnerabilities.controller;
 
+import org.fluentd.logger.FluentLogger;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
 public class pingController {
 
+    private static FluentLogger LOG = FluentLogger.getLogger("fluentd.test");
+
     @RequestMapping(value={"/ping"})
     public String returnPong(){
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put("from", "userA");
+        data.put("to", "userB");
+        LOG.log("follow", data);
         return "pong";
     }
+
 }
